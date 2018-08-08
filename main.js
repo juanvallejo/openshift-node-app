fc9b81e8701d75d94ae6ba1b36b90268cf9b0131
juanvallejo <jvallejo@redhat.com>
2018-08-08 15:07:40 -0400

    add pvc, add mysql npm dep

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'foo',
+  password : 'password',
+  database : 'nodeapp'
+});
+
+function tryConnect() {
+	connection.connect(function(err) {
+		if (err !== null) {
+			console.log("error connecting to database", err.toString());
+			return;
+		}
+
+		setup(connection);
+		console.log("connected to mysql on port 3306");
+	});
+}
+
+function setup(connection) {
+}
+
+
 var http = require('http');
 
 var app = http.createServer(function(req, res) {
@@ -6,3 +30,6 @@ var app = http.createServer(function(req, res) {
 
 console.log("Listening on port 8000");
 app.listen(8000, '0.0.0.0');
+
+// attempt to connect
+setTimeout(tryConnect, 8000);
